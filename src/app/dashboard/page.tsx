6d9a32d23b9b4df7649a692acbb741f1f62e1623
Oraculus <oraculus@lumina.local>
2026-02-14 23:54:00 +0400

    Polish /dashboard layout: header title, spacing, chart height

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { AppSidebar } from "@/components/app-sidebar"
 import { ChartAreaInteractive } from "@/components/chart-area-interactive"
 import { DataTable } from "@/components/data-table"
@@ -12,15 +14,19 @@ export default function Page() {
     <SidebarProvider>
       <AppSidebar variant="inset" />
       <SidebarInset>
-        <SiteHeader />
+        <SiteHeader title="Dashboard Overview" />
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-              <SectionCards />
               <div className="px-4 lg:px-6">
-                <ChartAreaInteractive />
+                <SectionCards />
               </div>
-              <DataTable data={data} />
+              <div className="px-4 lg:px-6">
+                <ChartAreaInteractive chartHeight={320} />
+              </div>
+              <div className="px-4 lg:px-6">
+                <DataTable data={data} />
+              </div>
             </div>
           </div>
         </div>
